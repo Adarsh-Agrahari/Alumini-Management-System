0521279f54bdef0fab7614df00e87e43c8f9a59e
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-11-16 03:11:00 +0530

    Update backend link

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: REACT_APP_API_URL, 
+  baseURL: 'https://alumini-management-system-be.vercel.app/api', 
   headers: {
     'Content-Type': 'application/json',
   },
