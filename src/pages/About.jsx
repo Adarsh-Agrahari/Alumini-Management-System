011e04ffda89b7843eeba6b2031dbed2d1d85254
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-11-15 18:22:24 +0530

    Add about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="bg-white py-16 mt-16">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-6">About Us</h2>
+        <p className="text-gray-600 text-center max-w-3xl mx-auto">
+          AlumniConnect is a platform to bring alumni together, fostering
+          connections and providing a space for professional and personal growth.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
