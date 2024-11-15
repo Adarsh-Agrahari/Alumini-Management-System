5a6cf8a7dc76e24e2f19757b8f7bcf81ac29e33c
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-11-15 18:21:47 +0530

    Add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 bg-opacity-80 backdrop-blur-md text-white fixed w-full top-0 z-10 px-4 py-3">
+
+      <div className="container mx-auto flex justify-between items-center">
+        <img src="img/tmslogo.png" alt="Logo" className="h-16" />
+        <h1 className="text-3xl font-bold">AlumniConnect</h1>
+        <ul className="flex space-x-4 text-lg">
+          <li><Link to="/" className="hover:bg-gray-700 hover:text-blue-400 py-2 px-3 rounded-lg">Home</Link></li>
+          <li><Link to="/about" className="hover:bg-gray-700 hover:text-blue-400 py-2 px-3 rounded-lg">About</Link></li>
+          <li><Link to="/gallery" className="hover:bg-gray-700 hover:text-blue-400 py-2 px-3 rounded-lg">Gallery</Link></li>
+          <li><Link to="/events" className="hover:bg-gray-700 hover:text-blue-400 py-2 px-3 rounded-lg">Events</Link></li>
+          <li><Link to="/alumni" className="hover:bg-gray-700 hover:text-blue-400 py-2 px-3 rounded-lg">Alumni</Link></li>
+          <li><Link to="/contact" className="hover:bg-gray-700 hover:text-blue-400 py-2 px-3 rounded-lg">Contact</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
