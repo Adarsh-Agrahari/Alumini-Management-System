714d30aaceb590deff62962911e37a239dba9902
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-11-15 18:22:07 +0530

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="bg-gray-800 bg-opacity-80 backdrop-blur-md text-white py-2">
+			<div className="container mx-auto text-center">
+				<p>&copy; 2024 AlumniConnect. All Rights Reserved.</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
