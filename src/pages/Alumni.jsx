ec8dac0bbfb28fa6cab7bc2df2791ad3989470bf
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-11-15 18:22:48 +0530

    Add alumini page

---
+++ src/pages/Alumni.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Alumni = () => {
+  return (
+    <section className="bg-white py-16 mt-16">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-6">Our Alumni</h2>
+        <p className="text-gray-600 text-center">
+          Meet and connect with alumni who have been a part of our journey.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Alumni;
