9c8b7a2dbbf49a90fbcabd33c046d0dedd8c0619
Adarsh Agrahari <adarshagrahari0503@gmail.com>
2024-11-15 18:24:11 +0530

    Setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
-function App() {
-	return <div className="bg-red-500 text-3xl">Alumini Management System</div>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import Alumni from "./pages/Alumni";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
+import Footer from "./components/Footer";
+
+const App = () => {
+	return (
+		<Router>
+			<div>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/gallery" element={<Gallery />} />
+					<Route path="/events" element={<Events />} />
+					<Route path="/alumni" element={<Alumni />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
+};
 
 export default App;
